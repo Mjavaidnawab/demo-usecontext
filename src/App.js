1d78a5d303a65fad6a0c745e1e74512791c7849d
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-08-15 11:45:18 +0400

    update usecontext to work with usestate

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import ValueContext from './ValueContext';
 
 function App() {
   //let [number, setNumber] = useState(45);
-let value = 81;
+//let value = 81;
+let value = useState(48);
   return (
     <ValueContext.Provider value ={value}>
 
--- src/Child.js
+++ src/Child.js
@@ -4,8 +4,11 @@ import ValueContext from './ValueContext';
 
 function Child() {
     let value = useContext(ValueContext);
+    console.log("value",value);
+    let updateValue = value[1];
   return (
-    <div> Child number { value }
+    <div> Child number { value[0] }
+    <button onClick ={()=> {updateValue(++value[0])}}> Update value </button>
     </div>
   );
 }
